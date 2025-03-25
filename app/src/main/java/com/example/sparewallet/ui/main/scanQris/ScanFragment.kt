243576af56c2ed0a01055514c243205d6ffb9889
Sanjaya Cahyadi Fuad <117884188+SanjayaCF@users.noreply.github.com>
2025-03-24 21:26:52 -0700

    change dashboard to qris

--- app/src/main/java/com/example/sparewallet/ui/main/scanQris/ScanFragment.kt
+++ app/src/main/java/com/example/sparewallet/ui/main/scanQris/ScanFragment.kt
@@ -1,4 +1,4 @@
-package com.example.sparewallet.ui.main.dashboard
+package com.example.sparewallet.ui.main.scanQris
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.sparewallet.databinding.FragmentDashboardBinding
+import com.example.sparewallet.databinding.FragmentScanBinding
 
-class DashboardFragment : Fragment() {
+class ScanFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentScanBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val scanViewModel =
+            ViewModelProvider(this).get(ScanViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentScanBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        scanViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/sparewallet/ui/main/scanQris/ScanViewModel.kt
+++ app/src/main/java/com/example/sparewallet/ui/main/scanQris/ScanViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.sparewallet.ui.main.dashboard
+package com.example.sparewallet.ui.main.scanQris
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ScanViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
