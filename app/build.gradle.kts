48dbfb5829262e6b045f8c14635afa8cde27ad07
Sanjaya Cahyadi Fuad <117884188+SanjayaCF@users.noreply.github.com>
2025-06-08 18:06:04 -0700

    add dependencies for md3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    id("org.jetbrains.kotlin.plugin.compose") version libs.versions.kotlin.get()
+
     alias(libs.plugins.google.gms.google.services)
 }
 
@@ -14,7 +17,6 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -27,6 +29,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -34,32 +37,47 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
+
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     implementation(libs.firebase.storage.ktx)
+
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.ui:ui:1.4.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    implementation("androidx.compose.material3:material3:1.1.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
+
+    implementation(libs.androidx.runtime.livedata)
+
+    implementation(libs.androidx.biometric)
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.github.yuriy-budiyev:code-scanner:2.3.0")
+    implementation("com.google.mlkit:barcode-scanning:17.0.3")
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(libs.androidx.biometric)
-    implementation(libs.material.v180)
-    implementation("com.squareup.picasso:picasso:2.8")
-    implementation("com.github.yuriy-budiyev:code-scanner:2.3.0")
-    implementation ("com.google.mlkit:barcode-scanning:17.0.3")
-
 }
